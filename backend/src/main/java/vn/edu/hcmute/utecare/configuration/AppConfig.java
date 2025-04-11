bf497cafa8f6212780aee8a601e2337135f6cb64
pchien925 <pcchien250904@gmail.com>
2025-04-11 21:58:44 +0700

    fix: fix-CORS

--- backend/src/main/java/vn/edu/hcmute/utecare/configuration/AppConfig.java
+++ backend/src/main/java/vn/edu/hcmute/utecare/configuration/AppConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -34,11 +35,11 @@ public class AppConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+            public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("*")
                         .allowedHeaders("*")
-                        .allowedOrigins("http://localhost:8080")
+                        .allowedOrigins("http://localhost:5173")
                         .allowCredentials(true)
                         .maxAge(3600);
             }
--- backend/src/main/java/vn/edu/hcmute/utecare/service/DoctorService.java
+++ backend/src/main/java/vn/edu/hcmute/utecare/service/DoctorService.java
@@ -5,6 +5,7 @@ import vn.edu.hcmute.utecare.dto.request.DoctorCreationRequest;
 import vn.edu.hcmute.utecare.dto.request.DoctorRequest;
 import vn.edu.hcmute.utecare.dto.response.DoctorResponse;
 import vn.edu.hcmute.utecare.dto.response.DoctorScheduleResponse;
+import vn.edu.hcmute.utecare.dto.response.DoctorScheduleSummaryResponse;
 import vn.edu.hcmute.utecare.dto.response.PageResponse;
 
 import java.time.LocalDate;
@@ -22,5 +23,5 @@ public interface DoctorService {
 
     PageResponse<DoctorResponse> searchDoctors(String keyword, int page, int size, String sort, String direction);
 
-    PageResponse<DoctorScheduleResponse> getDoctorAvailability(Long id, LocalDate date, int page, int size, String sort, String direction);
+    PageResponse<DoctorScheduleSummaryResponse> getDoctorAvailability(Long id, LocalDate date, int page, int size, String sort, String direction);
 }
